47ce7fee8a85c50d56946c0fce261ee352097ba8
Steve Lee <steve@opendirective.com>
2014-10-31 12:45:07 +0000

    Added licence and minor tweak

--- spikes/winsetres/winsetres/winsetres.cpp
+++ spikes/winsetres/winsetres/winsetres.cpp
@@ -1,3 +1,23 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// winsetres.cpp
+//
+// Copyright 2014 OpenDirective Ltd.
+//
+// Licensed under the New BSD license. You may not use this file except in
+// compliance with this License.
+//
+// You may obtain a copy of the License at
+// https://github.com/gpii/windows/blob/master/LICENSE.txt
+//
+// The research leading to these results has received funding from 
+// the European Union's Seventh Framework Programme (FP7/2007-2013) 
+// under grant agreement no. 289016.
+//
+// Spike to test changing resolution on Windows
+//
+///////////////////////////////////////////////////////////////////////////////
+
 #include <SDKDDKVer.h>
 #include <windows.h>
 #include <tchar.h>
@@ -42,7 +62,7 @@ static BOOL setScreenResolution(SIZE res)
 				dmCurrent.dmPelsWidth = width;
 				dmCurrent.dmPelsHeight = height;
 				long lRet = ChangeDisplaySettings(&dmCurrent, 0);
-				if (DISP_CHANGE_SUCCESSFUL == lRet)
+                if (DISP_CHANGE_SUCCESSFUL == lRet)
 				{
 					return TRUE;
 				}
@@ -64,7 +84,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	static wchar_t buf[100];
 
 	SIZE res = getScreenResolution();
-	swprintf_s(buf, _countof(buf), L"%u %u\r\n", res.cx, res.cy);
+	swprintf_s(buf, _countof(buf), L"Current: %u %u\r\n", res.cx, res.cy);
 	wprintf(buf);
 
 	if (argc > 2)
@@ -72,11 +92,11 @@ int _tmain(int argc, _TCHAR* argv[])
 		SIZE newRes = { _wtoi(argv[1]), _wtoi(argv[2]) };
 		BOOL r = setScreenResolution(newRes);
 		printf((r) ? "Set OK\r\n" : "Set Failed\r\n");
-	}
 
-	res = getScreenResolution();
-	swprintf_s(buf, _countof(buf), L"%u %u\r\n", res.cx, res.cy);
-	wprintf(buf);
+	    res = getScreenResolution();
+	    swprintf_s(buf, _countof(buf), L"New: %u %u\r\n", res.cx, res.cy);
+	    wprintf(buf);
+    }
 
 	return 0;
 }
